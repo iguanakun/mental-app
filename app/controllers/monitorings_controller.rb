91c73a26f50254700a84416454f4a0b909836acf
iguanakun <yw16027@gmail.com>
2023-10-28 22:19:23 +0900

    search削除

--- app/controllers/monitorings_controller.rb
+++ app/controllers/monitorings_controller.rb
@@ -65,19 +65,6 @@ class MonitoringsController < ApplicationController
     @q = data.ransack(params[:q])
     @monitorings = @q.result(distinct: true)
   end
-
-  def search
-    # params[:q]がnilではない且つ、params[:q][:name]がnilではないとき（商品名の欄が入力されているとき）
-    # if params[:q] && params[:q][:name]と同じような意味合い
-    if params[:q]&.dig(:tags_tag_name)
-      # squishメソッドで余分なスペースを削除する
-      squished_keywords = params[:q][:tags_tag_name].squish
-      ## 半角スペースを区切り文字として配列を生成し、paramsに入れる
-      params[:q][:tags_tag_name_cont_any] = squished_keywords.split(" ")
-    end
-    @q = current_user.monitorings.ransack(params[:q])
-    @monitorings = @q.result(distinct: true)
-  end
   
   private
 
