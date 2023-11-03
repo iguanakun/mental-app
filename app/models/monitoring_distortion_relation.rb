34d612b5d803d0d4fa0010a9fdc234452dfac419
iguanakun <yw16027@gmail.com>
2023-11-03 21:24:47 +0900

    最大値取得機能

--- app/models/monitoring_distortion_relation.rb
+++ app/models/monitoring_distortion_relation.rb
@@ -1,4 +1,24 @@
 class MonitoringDistortionRelation < ApplicationRecord
   belongs_to :monitoring
   belongs_to :distortion_list
+
+  def self.distortion_max_count(user_id)
+    # 特定のユーザーに関連づけられたmonitoring_idの最大値を取得
+    max_monitoring_ids = Monitoring.where(user_id: user_id).pluck(:id)
+
+    # 特定のユーザーに関連づけられたmonitoring_idと一致するレコードをフィルタリング
+    relevant_records = where(monitoring_id: max_monitoring_ids)
+
+    # distortion_list_idごとの出現回数をカウントして、降順でソート
+    dist_id_counts = relevant_records.group(:distortion_list_id).count.sort_by { |_, v| -v }.to_h
+
+    # valueが最大のkeyを取得
+    max_count = dist_id_counts.values.max
+    max_id = dist_id_counts.key(max_count)
+
+    # keyを持つレコードを取得
+    record = DistortionList.find_by(id: max_id)
+
+    return { distortion_name: record.distortion_name, max_count: max_count }
+  end
 end
--- app/models/monitoring_negative_feel.rb
+++ app/models/monitoring_negative_feel.rb
@@ -1,4 +1,24 @@
 class MonitoringNegativeFeel < ApplicationRecord
   belongs_to :monitoring
   belongs_to :negative_feel
+
+  def self.negative_max_count(user_id)
+    # 特定のユーザーに関連づけられたmonitoring_idの最大値を取得
+    max_monitoring_ids = Monitoring.where(user_id: user_id).pluck(:id)
+
+    # 特定のユーザーに関連づけられたmonitoring_idと一致するレコードをフィルタリング
+    relevant_records = where(monitoring_id: max_monitoring_ids)
+
+    # negative_feel_idごとの出現回数をカウントして、降順でソート
+    negative_id_counts = relevant_records.group(:negative_feel_id).count.sort_by { |_, v| -v }.to_h
+
+    # valueが最大のkeyを取得
+    max_count = negative_id_counts.values.max
+    max_id = negative_id_counts.key(max_count)
+
+    # keyを持つレコードを取得
+    record = NegativeFeel.find_by(id: max_id)
+
+    return { negative_feel_name: record.negative_feel_name, max_count: max_count }
+  end
 end
