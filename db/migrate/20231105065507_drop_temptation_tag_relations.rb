a616f157421517bf313a0b585a1f9fffbea52091
iguanakun <yw16027@gmail.com>
2023-11-06 12:24:10 +0900

    dropマイグレーション削除

--- db/migrate/20231105065507_drop_temptation_tag_relations.rb
+++
@@ -1,9 +0,0 @@
-class DropTemptationTagRelations < ActiveRecord::Migration[7.0]
-  def change
-    drop_table :temptation_tag_relations do |t|
-      t.references :temptation
-      t.references :tag
-      t.timestamps
-    end
-  end
-end
