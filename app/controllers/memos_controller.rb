710526634b7d43e51a0c68d7b520807d269f3307
iguanakun <yw16027@gmail.com>
2023-11-18 17:42:56 +0900

    感情統計機能を統合

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -5,5 +5,9 @@ class MemosController < ApplicationController
     @cbt_basics = current_user.cbt_basics.select('*, "cbt_basic" as data_type')
     @monitorings = current_user.monitorings.select('*, "monitoring" as data_type')
     @lists = (@cbt_basics + @monitorings).sort_by(&:created_at).reverse
+
+    # 上位３つ表示機能のため、メソッドを実行
+    @negative_feels = MaxNegativeService.negative_max_count(current_user)
+    @distortions = MonitoringDistortionRelation.distortion_max_count(current_user)
   end
 end
