b352fcd951fbbfd3c5a7875c2a70f9f7f769d3df
iguanakun <yw16027@gmail.com>
2023-11-28 15:43:16 +0900

    Update memos_controller.rb

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -2,12 +2,12 @@ class MemosController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    # @cbt_basics = current_user.cbt_basics.select('*, "cbt_basic" as data_type')
-    # @monitorings = current_user.monitorings.select('*, "monitoring" as data_type')
-    # @lists = (@cbt_basics + @monitorings).sort_by(&:created_at).reverse
+    @cbt_basics = current_user.cbt_basics.select('*, "cbt_basic" as data_type')
+    @monitorings = current_user.monitorings.select('*, "monitoring" as data_type')
+    @lists = (@cbt_basics + @monitorings).sort_by(&:created_at).reverse
 
-    # # 上位３つ表示機能のため、メソッドを実行
-    # @negative_feels = MaxNegativeService.negative_max_count(current_user)
-    # @distortions = MonitoringDistortionRelation.distortion_max_count(current_user)
+    # 上位３つ表示機能のため、メソッドを実行
+    @negative_feels = MaxNegativeService.negative_max_count(current_user)
+    @distortions = MonitoringDistortionRelation.distortion_max_count(current_user)
   end
 end
