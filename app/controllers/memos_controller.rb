d6068d8215e4bad2a92bc67340de746b30aaf2b6
iguanakun <yw16027@gmail.com>
2023-11-28 15:58:40 +0900

    Update memos_controller.rb

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -2,8 +2,8 @@ class MemosController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @cbt_basics = current_user.cbt_basics.select('*, "cbt_basic" as data_type')
-    @monitorings = current_user.monitorings.select('*, "monitoring" as data_type')
+    @cbt_basics = current_user.cbt_basics.select('*, "cbt_basic" as "data_type"')
+    @monitorings = current_user.monitorings.select('*, "monitoring" as "data_type"')
     @lists = (@cbt_basics + @monitorings).sort_by(&:created_at).reverse
 
     # 上位３つ表示機能のため、メソッドを実行
